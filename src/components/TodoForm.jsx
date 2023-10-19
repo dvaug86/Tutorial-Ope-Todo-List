3d0c94cfaf22ac602170ae510c04d8f110b517db
David Vaughan <dvaug86@gmail.com>
2023-10-19 10:11:01 -0400

    todo list now has ability to add tasks

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
   const [value, setValue] = useState("");
 
-const handleSubmit = e =>{
-  e.preventDefault();  //by default when you hit submit the page reloads so this e.preventDefault() function stops that
-  console.log(value)
-  
-}
+  const handleSubmit = (e) => {
+    e.preventDefault(); //by default when you hit submit the page reloads so this e.preventDefault() function stops that
+    addTodo(value);
+    setValue("");
+  };
 
   return (
     <form className="TodoForm" onSubmit={handleSubmit}>
@@ -15,6 +15,7 @@ const handleSubmit = e =>{
         type="text"
         className="todo-input"
         placeholder="What is the task today?"
+        value={value}
         onChange={(e) => setValue(e.target.value)}
       />
       <button type="submit" className="todo-btn">
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import TodoForm from './TodoForm'
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import Todo from "./Todo";
+
+uuidv4();
 
 const TodoWrapper = () => {
-  return (
-    <div className='TodoWrapper'>
-        <TodoForm/>
-    </div>
-  )
-}
+  const [todos, setTodos] = useState([]);
 
-export default TodoWrapper
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      {
+        id: uuidv4(),
+        task: todo,
+        completed: false,
+        isEditing: false,
+      },
+    ]); //'...' spreader function that makes a copy of the current state
+    console.log(todos);
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <h1>Get things Done!</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} />
+      ))}
+    </div>
+  );
+};
+
+export default TodoWrapper;
